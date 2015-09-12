8716e59d40dd27c6ed7bdb2757d90c3bde5282fb
lalawue <suchaaa@gmail.com>
2015-09-12 14:09:09 +0800

    merge submodule into one

---
+++ ios/MHttpServ/MHttpServ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MHttpServ
+//
+//  Created by lalawue on 15/9/12.
+//  Copyright (c) 2015年 onz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios/MHttpServ/MHttpServ/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MHttpServ
+//
+//  Created by lalawue on 15/9/12.
+//  Copyright (c) 2015年 onz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
