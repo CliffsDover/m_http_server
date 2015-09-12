63bf8d8c9c786d48bbd9f4269eb98c34cc50a1c5
lalawue <suchaaa@gmail.com>
2015-09-12 13:56:37 +0800

    listen on

--- src/client/client_http_serv.c
+++ src/client/client_http_serv.c
@@ -768,7 +768,7 @@ int main(int argc, char *argv[]) {
    }
 
    _info("\n");
-   _info("listen in http://127.0.0.1:1234\n");
+   _info("listen on http://127.0.0.1:1234\n");
    _info("\n");
 
    mnet_init();
